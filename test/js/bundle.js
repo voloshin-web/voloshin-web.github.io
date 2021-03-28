2009196882f2666a3099e1074b1728cde7cec4a0
voloshkin-v <voloshin.v.02@gmail.com>
2021-03-28 12:03:10 +0300

    test

---
+++ test/js/bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**************************!*\
+  !*** ./src/js/script.js ***!
+  \**************************/
+new WOW().init();
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
