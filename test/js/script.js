225e717f6ed73d652db426bc022f84e537752b8e
voloshkin-v <voloshin.v.02@gmail.com>
2021-03-29 23:30:58 +0300

    scroll

--- test/js/script.js
+++ test/js/script.js
@@ -1,40 +1,12 @@
 import Wow from 'wow.js';
 import Relax from 'rellax';
+import SmoothScroll from 'smooth-scroll';
 import menu from './menu';
 
 window.addEventListener('DOMContentLoaded', () => {
     new Wow().init();
     const relax = new Relax('.rellax');
+    let scroll = new SmoothScroll('a[href*="#"]');
 
     menu('.hamburger', '.menu', '.overlay');
-
-    (function() {
-        scrollTo();
-    })();
-    
-    function scrollTo() {
-        const links = document.querySelectorAll('.menu__link');
-        links.forEach(each => (each.onclick = scrollAnchors));
-    }
-    
-    function scrollAnchors(e, respond = null) {
-        const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
-        e.preventDefault();
-        var targetID = (respond) ? respond.getAttribute('href') : this.getAttribute('href');
-        const targetAnchor = document.querySelector(targetID);
-        if (!targetAnchor) {
-            return;
-        }
-        const originalTop = distanceToTop(targetAnchor);
-        window.scrollBy({ top: originalTop, left: 0, behavior: 'smooth' });
-        const checkIfDone = setInterval(function() {
-            const atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
-            if (distanceToTop(targetAnchor) === 0 || atBottom) {
-                targetAnchor.tabIndex = '-1';
-                targetAnchor.focus();
-                window.history.pushState('', '', targetID);
-                clearInterval(checkIfDone);
-            }
-        }, 100);
-    }
 });
