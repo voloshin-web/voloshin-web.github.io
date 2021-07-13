e292dcea253d1063793c495874c92ee32f0b314f
voloshkin-v <voloshin.v.02@gmail.com>
2021-07-13 17:28:17 +0300

    test commit

--- car/js/script.js
+++ car/js/script.js
@@ -1,100 +1,66 @@
+import Rellax from 'rellax';
+import Wow from 'wow.js';
+import SmoothScroll from 'smooth-scroll';
+import Glide from '@glidejs/glide';
+
+import menu from './modules/menu';
+import showmore from './modules/showmore';
+import input from './modules/input';
+import scrollEffect from './modules/scrollEffect';
+
+
 window.addEventListener('DOMContentLoaded', () => {
+    let rellax = new Rellax('.rellax');
+    new Wow().init(); 
+    let scroll = new SmoothScroll('a[href*="#"]');
+
+    const config = {
+        type: 'carousel',
+        perView: 4,
+        gap: 0,
+        perTouch: 1,
+        // breakpoints: {
+        //     1770: {
+        //         gap: 26
+        //     },
+        //     1200: {
+        //         gap: 12,
+        //         perView: 4
+        //     },
+        //     991: {
+        //         perView: 3,
+        //         gap: 12
+        //     },
+        //     600: {
+        //         perView: 2,
+        //         gap: 12
+        //     },
+        //     400: {
+        //         perView: 1,
+        //         gap: 0
+        //     }
+        // } 
+    };
+
+    new Glide('.glide', config).mount();
+    new Glide('.glide1', config).mount();
+    new Glide('.glide2', config).mount();
+    new Glide('.glide3', config).mount();
+    new Glide('.glide4', config).mount();
+    new Glide('.glide5', config).mount();
+    new Glide('.glide6', config).mount();
+    new Glide('.glide7', config).mount();
+
     // Меню 
-    const hamburger = document.querySelector('.header .hamburger'),
-          menu = document.querySelector('.menu'),
-          overlay = document.querySelector('.overlay'),
-          links = document.querySelectorAll('.menu .menu__link');
-
-    function toggleMenu() {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_active');
-        overlay.classList.toggle('overlay_active');
-    }
-
-    links.forEach(link => {
-        link.addEventListener('click', toggleMenu);
-    });
-
-    hamburger.addEventListener('click', toggleMenu);
-    overlay.addEventListener('click', toggleMenu);
+    menu();
 
     // Показать все.
-    const cars = document.querySelectorAll('.glide__slides'),
-        carsItem = document.querySelectorAll('.glide__slide'),
-        btns = document.querySelectorAll('.glide__arrows'),
-        showMore = document.querySelectorAll('.show');
-
-
-    showMore.forEach((link, i) => {
-        link.addEventListener('click', (e) => {
-            e.preventDefault();
-
-            cars[i].classList.toggle('show__more');
-            
-            cars[i].querySelectorAll('li').forEach(div => {
-                if (div.classList.contains('glide__slide--clone')) {
-                    div.style.display = 'none';
-                }
-
-                div.classList.toggle('glide__slide_active');
-            }); 
-
-
-            if (link.textContent === "Показати всі") {
-                link.textContent = 'Приховати';
-
-                btns[i].style.display = 'none';
-
-            } else {
-                link.textContent = 'Показати всі';
-
-                
-                cars[i].querySelectorAll('li').forEach(div => {
-                    if (div.classList.contains('glide__slide--clone')) {
-                        div.style.display = '';
-                    }
-                }); 
-
-                btns[i].style.display = '';
-                cars[i].style.transition = '';
-            }
-        });
-    }); 
-
+    showmore();
 
     // Только числа для INPUTA. 
-    const form = document.querySelectorAll('form'),
-          telInput = document.querySelectorAll('#tel');
-
-    telInput.forEach(input => {
-        input. addEventListener('input', () => {
-            input.value = input.value.replace(/\D/, '');
-        });
-    });
+    input();
 
     // Скролл эффект
-    let scrollPos = 0;
-
-    const car = document.querySelector('.car'),
-          list = document.querySelector('.stages__list');
-
-    car.classList.remove("car__up");
-    car.classList.remove("car__active");
-    list.classList.remove("stage__list__bottom");
-
-    window.addEventListener('scroll', () => { 
-        let st = window.pageYOffset;
-
-        if (st > scrollPos) {
-            car.classList.remove("car__up");
-            car.classList.add("car__active");
-        } else {
-            car.classList.add("car__up");
-            car.classList.remove("car__active");
-            list.classList.remove("stage__list__bottom");
-        }
-
-        scrollPos = st;
-    });
+    scrollEffect();
 }); 
 
